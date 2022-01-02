4a34dc31f3c5cab4331810edb1b8a48738e9b324
juhui <youduwngml@naver.com>
2022-01-02 16:47:09 +0900

    Nested for loop, Array including String

---
+++ 29 - 32.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+/******29.Rectangular*****/
+//int main(){
+//	int a,i,j;
+//	scanf("%d", &a);
+//	for(i=1;i<=a;i++)
+//	{
+//		for(j=1;j<=a;j++) printf("* ");
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+/******30.Triangle-1******/
+//int main() {
+//	int a,i,j;
+//	scanf("%d",&a);
+//	for(i=a;i>=1;i--)
+//	{
+//		for(j=1;j<=i;j++) printf("@");
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+/******31.Triangle-2******/
+//int main() {
+//	int a,i,j,k;
+//	scanf("%d",&a);
+//	for(i=a;i>=1;i--)
+//	{
+//		for(j=1;j<=i-1;j++) printf(" ");
+//		for(k=1;k<=a-i+1;k++) printf("@");
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+/******32.Triangle-3******/
+//int main(){
+//	int n, i, j, k;
+//	scanf("%d", &n);
+//	for(i=1; i<=n/2+1; i++)
+//	{
+//		for(k=i; k<n/2+1; k++) printf(" ");
+//		for(j=1; j<=i; j++) printf("@");
+//		printf("\n");
+//	}
+//	for(i=1; i<=n/2; i++)
+//	{
+//		for(k=1; k<=i; k++) printf(" ");
+//		for(j=i; j<=n/2; j++) printf("@");
+//		printf("\n");
+//	}	
+//	return 0;
+//}
+
+int main(){
+	
+	return 0;
+}
+
